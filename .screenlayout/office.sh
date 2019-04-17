a8e8078bed4dd569a44a674081456bbace056ef9
Alejandro Claro <alejandro.claro@gmail.com>
2019-04-17 10:41:48 +0200

    Add xrandr screen layout configuration files.

---
+++ .screenlayout/office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output HDMI-1 --mode 1680x1050 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 138x1050 --rotate normal --output HDMI-2 --off
